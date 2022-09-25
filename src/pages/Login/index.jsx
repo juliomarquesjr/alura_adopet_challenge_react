aa3a7c7863eb5b77d58f2dd37e7b7a83fa0c84b1
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-09-25 02:12:03 -0300

    Pagina de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 
-import Footer from "../../components/Footer";
 import Menu from "../../components/Menu";
 import Titulos from "../../components/Titulos";
 import Detalhe from "../../components/Detalhe";
-
-import Caes from "../../components/Caes"
+import Botao from "../../components/Botao";
+import Footer from "../../components/Footer"
 
 import "./styles.css";
 
@@ -17,11 +16,26 @@ const Login = () => {
   return (
     <React.Fragment>
       <Menu />
-      <Titulos
-        titulo=""
-        subtitulo="Ainda não tem cadastro?
-        Então, antes de buscar seu melhor amigo, precisamos de alguns dados:"
-      ></Titulos>
+      <Titulos titulo="" subtitulo="Ja tem conta? Faça seu login:">
+        <form className="formulario_login" action="#">
+          <label>Email</label>
+          <input
+            style={{ width: "100%" }}
+            type="email"
+            placeholder="Digite o seu melhor email"
+          />
+          <label>Senha</label>
+          <input
+            style={{ width: 362 }}
+            type="password"
+            placeholder="Digite sua senha"
+  
+          />
+        </form>
+        <div className="botao_login">
+          <Botao texto="Entrar" tamanho={174} />
+        </div>
+      </Titulos>
       <Detalhe />
       <Footer />
     </React.Fragment>
